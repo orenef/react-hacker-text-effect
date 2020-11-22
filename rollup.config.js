f04b4feb8e66405faa8209f805f96c7a38840944
orenef <oren.efraim@gmail.com>
2020-11-22 10:02:39 +0200

    Init project text effect

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import resolve from '@rollup/plugin-node-resolve'
+import external from 'rollup-plugin-peer-deps-external'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+import postcss from "rollup-plugin-postcss"
+
+import packageJson from './package.json'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      format: 'cjs',
+      file: packageJson.main,
+      exports: 'named',
+      sourcemap: true
+    },
+    {
+      format: 'es',
+      file: packageJson.module,
+      exports: 'named',
+      sourcemap: true
+    }
+  ],
+  external: [
+    'react',
+    'react-dom',
+  ],
+  plugins: [
+    resolve(),
+    external(),
+    commonjs({
+      include: ['node_modules/**'],
+    }),
+    typescript({
+      clean: true,
+      rollupCommonJSResolveHack: true,
+      exclude: ['node_modules'],
+    }),
+    postcss()
+  ]
+}
---
+++ src/playground/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HackText } from '../HackText'
+
+ReactDOM.render(
+  <HackText word="trick and try" textSize="28px"
+  // colors={{
+  //   textGlowColor: 'green',
+  //   textColor: 'gray',
+  //   textShadowColor: '#449e4f',
+  //   textGlowShadowColor: '#49e05c',
+  // }}
+  />,
+  document.getElementById('root'),
+);
